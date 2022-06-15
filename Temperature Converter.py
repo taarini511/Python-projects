d330c34effe3815eba06dbdf3ba0a949b0332f13
taarini511 <107341967+taarini511@users.noreply.github.com>
2022-06-15 18:13:37 +0530

    Add files via upload

---
+++ Temperature Converter.py
@@ -0,0 +1,9 @@
+celsius = float(input("Please enter the temperature in celsius: "))
+fahrenheit = (celsius * 1.8) + 32
+print("Temperature in fahrenheit is: ", fahrenheit)
+if(fahrenheit >= 104):
+  print("AAAAAAAAA It is too hooooooootttttt!!!!!") #I am Goldilocks
+elif(fahrenheit <= 50):
+    print("jdhvjdfhbvjebv It is too cold!!!!!") #Mai Goldilocks hu
+else:
+ print("What a pleasant day!") #Je suis Goldilocksure 
